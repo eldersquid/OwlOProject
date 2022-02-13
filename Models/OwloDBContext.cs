b8b173f2fe60c9d04d821d1d1dfe39a951a2605d
Lim Jing Kai <elljaykei@gmail.com>
2022-02-13 13:52:20 +0800

    idk

--- Models/OwloDBContext.cs
+++ Models/OwloDBContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using OwlOProjectA.Models;
 
 namespace OwlOProjectA.Models
 {
@@ -32,7 +33,5 @@ namespace OwlOProjectA.Models
         public DbSet<Owlee> Owlees { get; set; }
 
         public DbSet<Voucher> Vouchers { get; set; }
-        public DbSet<Workshop> Workshop { get; set; }
-        public DbSet<LuckyDraw> LuckyDraws { get; set; }
     }
 }
