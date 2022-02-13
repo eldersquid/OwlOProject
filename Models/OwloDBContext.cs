3014e1c1f70fea1ce01f408e7b92420bbbb48dc3
Lim Jing Kai <elljaykei@gmail.com>
2022-02-13 13:40:16 +0800

    Updated DBContext

--- Models/OwloDBContext.cs
+++ Models/OwloDBContext.cs
@@ -32,7 +32,7 @@ namespace OwlOProjectA.Models
         public DbSet<Owlee> Owlees { get; set; }
 
         public DbSet<Voucher> Vouchers { get; set; }
-
+        public DbSet<Workshop> Workshop { get; set; }
         public DbSet<LuckyDraw> LuckyDraws { get; set; }
     }
 }
